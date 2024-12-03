71426a94a3f0d1bb72704db1a0df88130707e0a9
rifaaulzah17 <rifana.az@almuslim.ac.id>
2024-12-03 11:07:45 +0700

    Add files via upload

---
+++ Frameku.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+class FramePertamaku extends JFrame {
+public FramePertamaku() {
+setTitle( "Frame Pertama" );
+setSize( 300 , 200 );
+}
+}
+public class Frameku {
+public static void main (String args[ ] ) {
+FramePertamaku coba = new FramePertamaku();
+coba.show();
+}
+} 
